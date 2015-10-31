1f01fe992e84cab9901a6a747a6c1eafa3693fe8
Jere Tapaninen <jere_tapaninen@hotmail.com>
2015-10-31 08:34:29 +0200

    Main Menu: created again.

---
+++ Armed Kart/Assets/Handlers/MainMenuButtonHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// Handles all the buttons in the main menu
+/// </summary>
+public class MainMenuButtonHandler : MonoBehaviour 
+{
+	/// <summary>
+	/// The next scene to navigate to after pressing the button
+	/// </summary>
+	public string NextScene;
+
+	/// <summary>
+	/// The starting method for this handler
+	/// </summary>
+	private void Start () 
+	{
+		GetComponent<Button> ().onClick.AddListener (() =>
+ 		{
+			if (NextScene == "Quit")
+			{
+				// quit the game
+				Application.Quit ();
+				return; // stop the application from continuing while in debugging mode
+			}
+
+			// load the level based on NextScene
+			Application.LoadLevel ("Scenes/" + NextScene);
+		});
+	}
+}
