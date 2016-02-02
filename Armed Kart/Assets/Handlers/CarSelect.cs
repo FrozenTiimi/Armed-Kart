a3396c4be01544bc7739565ae0750b133250ca45
Janne Jouni Tuhkanen <s3tuja01@students.osao.fi>
2016-02-02 13:36:46 +0200

    Talli on hieno :3

---
+++ Armed Kart/Assets/Handlers/CarSelect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSelect : MonoBehaviour 
+{
+
+	
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate (0, 0, 20 * Time.deltaTime);
+	}
+
+	public void ChangeCar(bool forward)
+	{
+		if (forward) 
+		{
+			
+		} 
+		else 
+		{
+
+		}
+	}
+}
